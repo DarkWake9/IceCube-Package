f9fcb9f19ed112724e30f5d951bfecb36d2de0ea
DarkWake9 <vibhavasu2018@gmail.com>
2023-07-07 01:39:28 +0530

    07072023 - 01.37 - UPDATE: PBS AND SLURM FILES UPDATED

--- package/slurm_all_cone.sh
+++ package/slurm_all_cone.sh
@@ -14,26 +14,26 @@
 cd /scratch/vibhavasu.phy.iith/IceCube-Package/package/task4s_pyformat_corrected_sig.py
 ## module load conda
 conda activate vibenv
-#SBATCH --output=t4s_3.out    ## Output file
-#SBATCH --error=t4s_3.err     ## Error file
+#SBATCH --output=t4s_3%j.out    ## Output file
+#SBATCH --error=t4s_3%j.err     ## Error file
 python3 ./task4s_pyformat_corrected_sig_term.py -c 3 -nc 48
 
 cd /scratch/vibhavasu.phy.iith/IceCube-Package/package/task4s_pyformat_corrected_sig.py
-#SBATCH --output=t4s_4.out    ## Output file
-#SBATCH --error=t4s_4.err     ## Error file
+#SBATCH --output=t4s_4%j.out    ## Output file
+#SBATCH --error=t4s_4%j.err     ## Error file
 python3 ./task4s_pyformat_corrected_sig_term.py -c 4 -nc 48
 
 cd /scratch/vibhavasu.phy.iith/IceCube-Package/package/task4s_pyformat_corrected_sig.py
-#SBATCH --output=t4s_5.out    ## Output file
-#SBATCH --error=t4s_5.err     ## Error file
+#SBATCH --output=t4s_5%j.out    ## Output file
+#SBATCH --error=t4s_5%j.err     ## Error file
 python3 ./task4s_pyformat_corrected_sig_term.py -c 5 -nc 48
 
 cd /scratch/vibhavasu.phy.iith/IceCube-Package/package/task4s_pyformat_corrected_sig.py
-#SBATCH --output=t4s_6.out    ## Output file
-#SBATCH --error=t4s_6.err     ## Error file
+#SBATCH --output=t4s_6%j.out    ## Output file
+#SBATCH --error=t4s_6%j.err     ## Error file
 python3 ./task4s_pyformat_corrected_sig_term.py -c 6 -nc 48
 
 cd /scratch/vibhavasu.phy.iith/IceCube-Package/package/task4s_pyformat_corrected_sig.py
-#SBATCH --output=t4s_7.out    ## Output file
-#SBATCH --error=t4s_7.err     ## Error file
+#SBATCH --output=t4s_7%j.out    ## Output file
+#SBATCH --error=t4s_7%j.err     ## Error file
 python3 ./task4s_pyformat_corrected_sig_term.py -c 7 -nc 48

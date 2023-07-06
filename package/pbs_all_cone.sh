87bade6a4194c345c7c12a4e709989dc204c3366
DarkWake9 <vibhavasu2018@gmail.com>
2023-07-07 01:37:40 +0530

    07072023 - 01.37 - UPDATE: PBS AND SLURM FILES UPDATED

--- package/pbs_all_cone.sh
+++ package/pbs_all_cone.sh
@@ -8,28 +8,28 @@ cd /scratch/shantanu/icecube/IceCube-Package/package
 #PBS -e task4s_3deg.err
 #PBS -o task4s_3deg.out
 
-python3 ./task4s_pyformat_corrected_sig_term.py -c 3 --nc 32
+python3 ./task4s_pyformat_corrected_sig_term.py -c 3 -nc 32
 
 cd /scratch/shantanu/icecube/IceCube-Package/package
 #PBS -e task4s_4deg.err
 #PBS -o task4s_4deg.out
 
-python3 ./task4s_pyformat_corrected_sig_term.py -c 4 --nc 32
+python3 ./task4s_pyformat_corrected_sig_term.py -c 4 -nc 32
 
 cd /scratch/shantanu/icecube/IceCube-Package/package
 #PBS -e task4s_5deg.err
 #PBS -o task4s_5deg.out
 
-python3 ./task4s_pyformat_corrected_sig_term.py -c 5 --nc 32    
+python3 ./task4s_pyformat_corrected_sig_term.py -c 5 -nc 32    
 
 cd /scratch/shantanu/icecube/IceCube-Package/package
 #PBS -e task4s_6deg.err
 #PBS -o task4s_6deg.out
 
-python3 ./task4s_pyformat_corrected_sig_term.py -c 6 --nc 32
+python3 ./task4s_pyformat_corrected_sig_term.py -c 6 -nc 32
 
 cd /scratch/shantanu/icecube/IceCube-Package/package
 #PBS -e task4s_7deg.err
 #PBS -o task4s_7deg.out
 
-python3 ./task4s_pyformat_corrected_sig_term.py -c 7 --nc 32
+python3 ./task4s_pyformat_corrected_sig_term.py -c 7 -nc 32
--- package/slurm_all_cone.sh
+++ package/slurm_all_cone.sh
@@ -16,24 +16,24 @@ cd /scratch/vibhavasu.phy.iith/IceCube-Package/package/task4s_pyformat_corrected
 conda activate vibenv
 #SBATCH --output=t4s_3.out    ## Output file
 #SBATCH --error=t4s_3.err     ## Error file
-python3 ./task4s_pyformat_corrected_sig_term.py -c 3 --nc 48
+python3 ./task4s_pyformat_corrected_sig_term.py -c 3 -nc 48
 
 cd /scratch/vibhavasu.phy.iith/IceCube-Package/package/task4s_pyformat_corrected_sig.py
 #SBATCH --output=t4s_4.out    ## Output file
 #SBATCH --error=t4s_4.err     ## Error file
-python3 ./task4s_pyformat_corrected_sig_term.py -c 4 --nc 48
+python3 ./task4s_pyformat_corrected_sig_term.py -c 4 -nc 48
 
 cd /scratch/vibhavasu.phy.iith/IceCube-Package/package/task4s_pyformat_corrected_sig.py
 #SBATCH --output=t4s_5.out    ## Output file
 #SBATCH --error=t4s_5.err     ## Error file
-python3 ./task4s_pyformat_corrected_sig_term.py -c 5 --nc 48
+python3 ./task4s_pyformat_corrected_sig_term.py -c 5 -nc 48
 
 cd /scratch/vibhavasu.phy.iith/IceCube-Package/package/task4s_pyformat_corrected_sig.py
 #SBATCH --output=t4s_6.out    ## Output file
 #SBATCH --error=t4s_6.err     ## Error file
-python3 ./task4s_pyformat_corrected_sig_term.py -c 6 --nc 48
+python3 ./task4s_pyformat_corrected_sig_term.py -c 6 -nc 48
 
 cd /scratch/vibhavasu.phy.iith/IceCube-Package/package/task4s_pyformat_corrected_sig.py
 #SBATCH --output=t4s_7.out    ## Output file
 #SBATCH --error=t4s_7.err     ## Error file
-python3 ./task4s_pyformat_corrected_sig_term.py -c 7 --nc 48
+python3 ./task4s_pyformat_corrected_sig_term.py -c 7 -nc 48
